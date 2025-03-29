aef29496f296539f7d4e60095d60ee27cac1f06f
joshhoang <loclhse172459@fpt.edu.vn>
2025-03-30 00:30:18 +0700

    changes in commit

--- src/main/java/com/example/Swp_Project/JwtUtils/JwtAuthenticationFilter.java
+++ src/main/java/com/example/Swp_Project/JwtUtils/JwtAuthenticationFilter.java
@@ -82,8 +82,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 || path.startsWith("/api/vaccinedetails-all")
                 || path.startsWith("/vaccines/{vaccineId}/details")
                 || path.equals("/api/cart/return") || (path.startsWith("/api/cart/return"))
-                || path.startsWith("/api/news-getall")
-                || path.startsWith("/api/news-getById/{id}");
+                || path.startsWith("/api/news-getall") || (path.startsWith("/api/news-getById/{id}")) ;
     }
 }
 
